539173c713e3cbba0c731e7d0a95b7e30d8b8b22
Utkirbek <utkirbek.me@gmail.com>
2022-10-13 02:21:21 +0500

    extra auth deleted

--- config/auth.js
+++ config/auth.js
@@ -20,18 +20,7 @@ const signInToken = (user) => {
   );
 };
 
-const tokenForVerify = (user) => {
-  return jwt.sign(
-    {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      password: user.password,
-    },
-    process.env.JWT_SECRET_FOR_VERIFY,
-    { expiresIn: '15m' }
-  );
-};
+
 
 const isAuth = async (req, res, next) => {
   const { authorization } = req.headers;
@@ -58,51 +47,13 @@ const isAdmin = async (req, res, next) => {
   }
 };
 
-const sendEmail = (body, res, message) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    service: process.env.SERVICE, //comment this line if you use custom server/domain
-    port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
 
-    //comment out this one if you usi custom server/domain
-    // tls: {
-    //   rejectUnauthorized: false,
-    // },
-  });
 
-  transporter.verify(function (err, success) {
-    if (err) {
-      res.status(403).send({
-        message: `Error happen when verify ${err.message}`,
-      });
-      console.log(err.message);
-    } else {
-      console.log('Server is ready to take our messages');
-    }
-  });
-
-  transporter.sendMail(body, (err, data) => {
-    if (err) {
-      res.status(403).send({
-        message: `Error happen when sending email ${err.message}`,
-      });
-    } else {
-      res.send({
-        message: message,
-      });
-    }
-  });
-};
 
 module.exports = {
   signInToken,
-  tokenForVerify,
+
   isAuth,
   isAdmin,
-  sendEmail,
+
 };
