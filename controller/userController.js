ed44a4b6b7a1e195ea625b646b079098390a4189
Utkirbek <utkirbek.me@gmail.com>
2022-11-21 20:55:30 +0500

    some chnages

--- controller/userController.js
+++ controller/userController.js
@@ -76,7 +76,7 @@ const searchUser = async (req, res) => {
     if (req.params.name) {
       const user = await User.find({
         name: {
-          $regex: new RegExp(req.params.name, "i"),git 
+          $regex: new RegExp(req.params.name, "i"),
         },
       });
       res.send(user);
